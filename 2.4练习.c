06b251c50a1c02dc8ac7723de25f463da6e31f80
zNine0414 <72492197+zNine0414@users.noreply.github.com>
2020-10-07 16:37:46 +0800

    Add files via upload

---
+++ 2.4练习.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void)
+{
+	 int i, j, k;
+  float x, y, z;
+
+  printf("Value of i: %d\n", i);
+  printf("Value of j: %d\n", j);
+  printf("Value of k: %d\n", k);
+
+  printf("Value of x: %g\n", x);
+  printf("Value of y: %g\n", y);
+  printf("Value of z: %g\n", z);
+
+  return 0;
+} 
